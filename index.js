482bb3fbc8f3c36fc1b54dafc7c451ef40a1ca2a
UktamHudoyaorv <uktamyb@gmail.com>
2021-08-03 18:15:12 +0500

    oxirgi change

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 // DOM
 
-const titleId = document.getElementById("title").innerHTML
-console.log(titleId);
+const titleID = document.getElementById("title");
+titleID.style.color = "red";
+titleID.style.cssText = "font-size: 10px";
+const body = document.querySelector("body");
+// body.style.background = "grey";
+// console.log(titleID.parentNode);
+// console.log(titleID.childNode);
+
+function changeTitle() {
+  titleID.style.cssText = `
+    color: red;
+    background: yellow;
+    font-size: 20px;
+  `;
+}
